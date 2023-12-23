c775b4eba289c169498e18294faa67bf16924c53
takasqr <95556025+takasqr@users.noreply.github.com>
2023-12-23 22:31:06 +0900

    GTM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,23 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content'
   ],
+  app: {
+    head: {
+      script: [
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','${process.env.GTM_ID}');` 
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          tagPosition: 'bodyOpen'
+        }
+      ]
+    }
+  }
 })
